2e37086a92f98fbcbd559f87c0a6cc16b18bfe7b
Abhinav Aryan <coolpadrocx@gmail.com>
2024-01-25 19:41:48 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import AddFields from './components/AddFields';
+import DisplayFields from './components/DisplayFields';
+import DynamicDisplayScreen from './components/DynamicDataCollection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div>
+        <AddFields />
+        <DisplayFields />
+        <DynamicDisplayScreen/>
+      </div>
+    </Provider>
+  ); 
 }
 
 export default App;
